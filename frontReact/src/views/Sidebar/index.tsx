45b2f13827c3dc1a25db290a5d5522da0fbc1e80
libdefi <libcreatesweb3@gmail.com>
2024-02-15 10:34:37 +0800

    Sidebar screen placement settings

--- frontReact/src/views/Sidebar/index.tsx
+++ frontReact/src/views/Sidebar/index.tsx
@@ -15,19 +15,29 @@ const Sidebar: React.FC = () => {
   };
 
   return (
-    <div className="dnd-flow-sidebar">
-      <div className="sidebar-body">
+    <div className="dnd-flow-sidebar flex flex-col justify-between min-h-screen">
+      <div className="flex items-center justify-between p-2 border-b border-gray-200">
+        <button className="px-4 py-2 bg-blue-500 text-white rounded-xl">Points</button>
+        <ConnectButton />
+      </div>
+      <div className="sidebar-body flex-grow">
         <div className="sidebar-nodes">
           {Object.entries(nodes).map(([key, node], i) => (
-            <div key={key} className="node-item" onDragStart={(event) => onDragStart(event, node.key)} draggable>
-              <span className="node-emoji">{node.emoji}</span>
-              <span className="node-label">{node.label}</span>
+            <div
+              key={key}
+              className="relative border border-gray-300 bg-white p-2 m-1 rounded-md overflow-hidden"
+              onDragStart={(event) => onDragStart(event, node.key)}
+              draggable
+            >
+              <span className="font-bold">{node.emoji}</span>
+              <span className="font-bold">{node.label}</span>
             </div>
           ))}
         </div>
       </div>
-      {/* TODO: UI fixed */}
-      {/* <ConnectButton /> */}
+      <div className="p-4 text-center border-t border-gray-200 flex items-center justify-center h-16">
+        <div className="text-gray-500">You earned 0.1 ETH ⛽️</div>
+      </div>
     </div>
   );
 };
