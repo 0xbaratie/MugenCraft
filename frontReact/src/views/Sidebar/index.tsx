6e3bda67a9bf03cccea6978c124b92de80a042c1
libdefi <libcreatesweb3@gmail.com>
2024-02-15 11:01:37 +0800

    Update index.tsx

--- frontReact/src/views/Sidebar/index.tsx
+++ frontReact/src/views/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Sidebar.scss';
 import { useNodeContext } from '../Nodes/NodeContext';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
@@ -6,6 +6,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 const tapSound = new Audio('/se/tap.mp3');
 
 const Sidebar: React.FC = () => {
+  const [showDetails, setShowDetails] = useState(false);
   const { nodes } = useNodeContext();
 
   const onDragStart = (event: React.DragEvent<HTMLDivElement>, nodeType: string) => {
@@ -16,8 +17,23 @@ const Sidebar: React.FC = () => {
 
   return (
     <div className="dnd-flow-sidebar flex flex-col justify-between min-h-screen">
-      <div className="flex items-center justify-between p-2 border-b border-gray-200">
-        <button className="px-4 py-2 bg-gray-400 text-white rounded-xl">Points</button>
+      <div className="relative flex items-center justify-between p-2 border-b border-gray-200">
+        <button className="px-4 py-2 bg-gray-400 text-white rounded-xl" onClick={() => setShowDetails(!showDetails)}>
+          Points
+        </button>
+        {showDetails && (
+          <div className="absolute top-full w-full p-4 rounded-lg shadow-lg border border-gray-200 z-50 bg-gray-50">
+            <span className="text-md">Your points</span>
+            {/* TODO: Change numbers dynamically */}
+            <p className="text-xl font-bold pb-4 border-b border-gray-200">150,000 points</p>
+            <div className="mt-4">
+              <div>20 Object minted | 5,000</div>
+              <div>5 Recipe Created | 10,000</div>
+              <div>200 Your recipe minted | 20,000</div>
+              <div>400 Your recipe assist | 4,000</div>
+            </div>
+          </div>
+        )}
         <ConnectButton />
       </div>
       <div className="sidebar-body flex-grow">
@@ -36,6 +52,7 @@ const Sidebar: React.FC = () => {
         </div>
       </div>
       <div className="p-4 text-center border-t border-gray-200 flex items-center justify-center h-16">
+        {/* TODO: Change numbers dynamically */}
         <div className="text-gray-500">You earned 0.1 ETH ⛽️</div>
       </div>
     </div>
