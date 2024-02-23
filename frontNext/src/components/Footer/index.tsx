38ee369e74c1af2fb1e10c3a6de88337220dfa57
libdefi <libcreatesweb3@gmail.com>
2024-02-24 01:28:33 +0800

    Up to 3 emoji and  delete function

--- frontNext/src/components/Footer/index.tsx
+++ frontNext/src/components/Footer/index.tsx
@@ -1,14 +1,17 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { useState, useEffect } from "react";
 import { Node } from "reactflow";
 import EmojiPicker, { EmojiClickData } from "emoji-picker-react";
 import { useToast } from "@/components/ui/use-toast"
+import Image from 'next/image'
+
 
 interface FooterProps {
   nodeA: Node | undefined;
   nodeB: Node | undefined;
   footerInput: { label: string; emoji: string };
   setFooterInput: React.Dispatch<
-    React.SetStateAction<{ label: string; emoji: string }>
+    React.SetStateAction<{ label: string; emoji: string[] }>
   >;
   updateNodeFromFooter: () => void;
 }
@@ -24,6 +27,7 @@ const Footer: React.FC<FooterProps> = ({
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
   const [validationMessage, setValidationMessage] = useState("");
   const { toast } = useToast()
+  
 
   useEffect(() => {
     // Disable buttons by default
@@ -59,10 +63,11 @@ const Footer: React.FC<FooterProps> = ({
   if (!nodeA || !nodeB) return null;
 
   return (
-    <div>
-      <div className="left-12 bottom-0 bg-white shadow-md p-4 flex justify-between items-center z-10">
-        <div className="flex items-center space-x-4">
-          <div className="flex items-center border border-blue-gray-100 bg-white rounded-md">
+    <>
+      <p className="ml-4 font-bold text-gray-400">Point chance! Let's define a new recipe to earn 1000 points!</p>
+      <div className="left-12 bottom-0 bg-white shadow-md px-4 pb-4 flex justify-between items-center z-10">
+        <div className="relative flex items-center justify-center space-x-4">
+          <div className="flex items-center border border-blue-gray-100 bg-gray-100 rounded-md">
             {nodeA.data.label ? (
               <div className="p-2">
                 <span className="font-bold">{`${nodeA.data.emoji}${nodeA.data.label}`}</span>
@@ -70,7 +75,7 @@ const Footer: React.FC<FooterProps> = ({
             ) : null}
           </div>
           <span className="flex items-center">+</span>
-          <div className="flex items-center border border-blue-gray-100 bg-white rounded-md">
+          <div className="flex items-center border border-blue-gray-100 bg-gray-100 rounded-md">
             {nodeB.data.label ? (
               <div className="p-2">
                 <span className="font-bold">{`${nodeB.data.emoji}${nodeB.data.label}`}</span>
@@ -79,24 +84,72 @@ const Footer: React.FC<FooterProps> = ({
           </div>
         </div>
         <span className="flex items-center mx-2">=</span>
-        <button
-          onClick={() => setShowEmojiPicker(!showEmojiPicker)} // Toggle visibility on click
-          className="border border-gray-300 hover:bg-gray-300 text-white font-bold py-2 px-4 rounded"
-        >
-          {footerInput.emoji ? footerInput.emoji : "🌏"}
+
+        <button className="flex items-center justify-between border border-gray-300 text-white font-bold py-2 px-4 rounded min-w-40">
+          <div>
+            {footerInput.emoji.length > 0 ? footerInput.emoji : "🌏"}
+          </div>
+
+          {/* 画像を右寄せにするためのコンテナ */}
+          <div className="flex-shrink-0 flex items-center">
+            <Image
+              src="/svg/emoji-smile.svg"
+              alt="Smile emoji"
+              width="20"
+              height="20"
+              onClick={(e) => {
+                e.stopPropagation();
+                setShowEmojiPicker(!showEmojiPicker);
+              }}
+              className="hover:fill-blue-500"
+            />
+            <Image
+              src="/svg/trash.svg"
+              alt="Delete emoji"
+              width="20"
+              height="20"
+              onClick={(e) => {
+                e.stopPropagation(); // 別のイベントハンドラーがあればここに追加
+                setFooterInput((prev) => ({
+                  ...prev,
+                  emoji: prev.emoji.slice(0, -1),
+                }));
+              }}
+              className="hover:fill-blue-500 ml-2"
+            />
+          </div>
         </button>
+
 
         {showEmojiPicker && (
           <div className="fixed left-12 bottom-0 bg-white shadow-md p-4 flex justify-between items-center z-100">
             <EmojiPicker
+              // EmojiPickerのonEmojiClickイベントハンドラ
               onEmojiClick={(emojiData: EmojiClickData, event: MouseEvent) => {
-                setFooterInput((prev) => ({ ...prev, emoji: emojiData.emoji }));
+                setFooterInput((prev) => {
+                  // 絵文字配列が3つ未満の場合のみ、新しい絵文字を追加
+                  if (prev.emoji.length < 3) {
+                    return {
+                      ...prev,
+                      emoji: [...prev.emoji, emojiData.emoji],
+                    };
+                  } else {
+                    // 絵文字配列が3つの場合、トーストメッセージを表示して現在の状態を維持
+                    toast({
+                      title: "Input alert🚨",
+                      description: "Maximum of 3 emojis",
+                    });
+                    return prev;
+                  }
+                });
                 setShowEmojiPicker(false);
               }}
+
               autoFocusSearch={false}
             />
           </div>
         )}
+        
         <input
           type="text"
           name="label"
@@ -119,7 +172,7 @@ const Footer: React.FC<FooterProps> = ({
         </button>
       </div>
       
-    </div>
+    </>
   );
 };
 
